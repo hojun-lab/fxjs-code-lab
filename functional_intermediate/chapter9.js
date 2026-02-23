4ee46fdb65773a6720972cdff28ee2e71b8fdf1d
rojojun <zoloman316@gmail.com>
2026-02-23 22:40:43 +0900

    Promise는 일급으로 활용해서 값으로 다루어진다.

--- functional_intermediate/chapter9.js
+++ functional_intermediate/chapter9.js
@@ -16,3 +16,22 @@ add20(5)
     .then(add20)
     .then(add20)
     .then(log);
+// 비동기 상황을 1급 값으로 다룸
+
+var a = add10(5, res => {
+    add10(res, res => {
+        log(res);
+    })
+})
+
+var b = add20(5)
+    .then(add20)
+    .then(add20)
+    .then(log);
+
+log(a)
+log(b)
+
+// a는 실횅 후 더 이상 할 수 있는 작업이 없음
+// b는 실행 후 다른 작업을 then 을 통해서 가능
+
